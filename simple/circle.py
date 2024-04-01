ce05452fe086ed539af660e300d9b834884845e7
maytheu <maytheu98@gmail.com>
2024-04-01 09:51:06 +0100

    shapes area

---
+++ simple/circle.py
@@ -0,0 +1,4 @@
+print("calculate the area of a circle")
+radius = float(input("Enter the circle radius = "))
+area = 3.142 * radius * radius
+print("Area of circle = " + str(area))
---
+++ simple/rect.py
@@ -0,0 +1,5 @@
+print("Calculate the area of a rectangle")
+a = int(input("Enter first side = "))
+b = int(input("Enter second side = "))
+area = a * b
+print("Area of rectangle = " + str(area))
